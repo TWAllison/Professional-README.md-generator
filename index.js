9cfb0cfa061cd7e76a8cf31cf9e35f1ebd21efac
TWAllison <tylerallison55@gmail.com>
2021-10-21 04:12:04 -0600

    create all promt questions and checkbox for license && test index.js

--- index.js
+++ index.js
@@ -18,23 +18,121 @@ const promtQuestions = ([
                 return false;
             }
         }
-    }
-
-    // Project Title
-
+    },
+     // Project Title
+     {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title.',
+        validate:titleInput => {
+            if (titleInput){
+                return true;
+            } else {
+                console.log ('Please enter your project Title!');
+                return false;
+            }
+        }
+    },
     // Project Description
-
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a project description.',
+        validate: descriptionInput => {
+            if (descriptionInput){
+                return true;
+            } else {
+                console.log ('Please enter your project description!');
+                return false;
+            }
+        }
+    },
     // Installation instructions
-
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter instructions on how to install your project.',
+        validate: installInput => {
+            if (installInput){
+                return true;
+            } else {
+                console.log ('Please enter installation instructions!');
+                return false;
+            }
+        }
+    },
     // How to Use the app 
-
+    {
+        type: 'input',
+        name: 'use',
+        message: 'How do you use this App?',
+        validate: useInput => {
+            if (useInput){
+                return true;
+            } else {
+                console.log ('Please describe how to use the App!');
+                return false;
+            }
+        }
+    },
     // Contribution instructions 
-
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can other Users contribute to your project?',
+        validate: contributeInput => {
+            if (contributeInput){
+                return true;
+            } else {
+                console.log ('Please describe how others can contribute to your project!');
+                return false;
+            }
+        }
+    },
     // test information
-
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can you test this project?',
+        validate: testInput => {
+            if (testInput){
+                return true;
+            } else {
+                console.log ('Please describe how to test this project!');
+                return false;
+            }
+        }
+    },
     // License (Mozilla-Public, Apache, MIT, or None) use checkbox instead of input 
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose the appropriate license for your project.',
+        choices: ['Mozilla-Public', 'Apache', 'Mit', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log ('Choose a License for your Project!');
+                return false;
+            }
+        }
+    },
 
     // Email adress to contact with further questions 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.',
+        validate: emailInput => {
+            if (emailInput){
+                return true;
+            } else {
+                console.log ('Please enter your email address!');
+                return false;
+            }
+        }
+    }
 ])
 
 // TODO: Create a function to write README file
