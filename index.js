58df57b61fbd44117ee660d607f0487c5475ef25
TWAllison <tylerallison55@gmail.com>
2021-10-21 02:45:37 -0600

    add require statements to index.js Sudo code prompt questions

---
+++ index.js
@@ -0,0 +1,35 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = requuire('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const promtQuestions = ([
+    // github user name
+    
+
+    // Project Title
+
+    // Project Description
+
+    // Installation instructions
+
+    // How to Use the app 
+
+    // Contribution instructions 
+
+    // test information
+
+    // License (Mozilla-Public, Apache, MIT, or None) use checkbox instead of input 
+
+    // Email adress to contact with further questions 
+])
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
